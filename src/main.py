892c07a69bae3471526095bbbabb84158ac7a962
Squirrel <squirrelhomebrew@gmail.com>
2025-03-10 23:07:22 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+SGC++ Main
+
+The main file for the SGC++ language. Idk what else really but yea it's just main.
+"""
+import sys
+import sgc
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        sgc = sgc.interpreter() 
+        sgc.run_file(sys.argv[1])
+        print("----------------------------------------------")
+        input("Press enter to exit..")
+    else:
+        print("Uhh so no file, use like: python main.py filename.sgc")
