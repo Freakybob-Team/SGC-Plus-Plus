a7291cd2005dc7043fe3bb1aa626733526cfe9a1
Squirrel <squirrelhomebrew@gmail.com>
2025-03-27 17:24:29 -0700

    Update begin.py

--- begin.py
+++ begin.py
@@ -1,10 +1,15 @@
 import os
 from pathlib import Path
+
 print("Welcome to SGC++! (aka SigmaGreg Code++, sgcx)")
-print("We'd like to thank you for updating or beginning to use SigmaGreg Code++ :)! We really apperciate it.")
+print("We'd like to thank you for updating or beginning to use SigmaGreg Code++ :)! We really appreciate it.")
 print("Please input the FULL path to main.bat (excluding the actual main.bat part) excluding quotation marks!")
+
 pathtobat = input()
+
 p = Path(pathtobat)
-p_no_path = pathtobat
+
 print(p)
-os.system('start "Setup SGCX" cmd /k "cd ' + p_no_path + '& .\main.bat"')
+
+
+os.system(f'start "Setup SGCX" cmd /k "cd /d {p} & .\\main.bat"')
